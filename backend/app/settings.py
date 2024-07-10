92ec09037edacd65c6c405f61f4b1a85f38a19dd
Fuad <fuadadio75@gmail.com>
2024-07-10 05:13:58 +0100

    feat: adds client urls to cors allowed origins

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'djoser',
+    'corsheaders',
 
     'api',
 ]
@@ -49,12 +50,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS= ["http://127.0.0.1:5173"]
 
 ROOT_URLCONF = 'app.urls'
 
